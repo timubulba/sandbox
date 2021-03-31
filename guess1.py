20b71f4d203377dc3543ba870d24a4e26f312df5
˝Tymofii <timubulba@gmail.com>
2021-03-31 18:32:31 +0300

    First guessing game

---
+++ guess1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import random
+
+print('I thought of a number, guess it!')
+
+n = random.randrange(1, 100, 1)
+
+while True:
+    s = input('> ')
+    a = int(s)
+    if a == n:
+        print('You guessed right!')
+        break
+    elif a < n:
+        print('Too low')
+    else:
+        print('Too hight')
